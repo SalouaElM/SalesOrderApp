aaa3577a19e3e05503a4be2bcd106f64480312ff
SalouaElM <91462849+elmsaloua@users.noreply.github.com>
2023-11-24 21:07:35 +0100

    initial commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["ap/salesorderapp/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"ap/salesorderapp/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
