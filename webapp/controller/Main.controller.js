4d1857611d38af6999e4fcd99f9fe2a7cff574f3
SalouaElM <91462849+elmsaloua@users.noreply.github.com>
2023-11-25 13:58:18 +0100

    Added filtering on document type and created by

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -4,11 +4,12 @@ sap.ui.define([
     'sap/f/library',
     'sap/ui/model/Sorter',
     'sap/ui/core/Fragment',
+    'sap/ui/model/Filter',
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, Device, fioriLibrary, Sorter, Fragment) {
+    function (Controller, Device, fioriLibrary, Sorter, Fragment, Filter) {
         "use strict";
 
         return Controller.extend("ap.salesorderapp.controller.Main", {
@@ -23,11 +24,29 @@ sap.ui.define([
                 oFCL.setLayout(fioriLibrary.LayoutType.TwoColumnsMidExpanded);
 
             },
-            handleSortButtonPressed: function () {
-                this.getViewSettingsDialog("ap.salesorderapp.fragments.sortDialog")
+            handleFilterButtonPressed: function () {
+                this.getViewSettingsDialog("ap.salesorderapp.fragments.filterDialog")
                     .then(function (oViewSettingsDialog) {
                         oViewSettingsDialog.open();
                     });
+            },
+            handleFilterDialogConfirm: function (oEvent) {
+                var oTable = this.byId("salesorderTable"),
+                    mParams = oEvent.getParameters(),
+                    oBinding = oTable.getBinding("items"),
+                    aFilters = [];
+
+                mParams.filterItems.forEach(function(oItem) {
+                    let sPath = oItem.getParent().getKey(),
+                        sOperator = 'EQ', 
+                        sValue1 = oItem.getKey(),
+                        oFilter = new Filter(sPath, sOperator, sValue1);
+                    aFilters.push(oFilter);
+                });
+
+                // apply filter settings
+                oBinding.filter(aFilters);
+
             },
             getViewSettingsDialog: function (sDialogFragmentName) {
                 var pDialog = this._mViewSettingsDialogs[sDialogFragmentName];
@@ -46,6 +65,12 @@ sap.ui.define([
                     this._mViewSettingsDialogs[sDialogFragmentName] = pDialog;
                 }
                 return pDialog;
+            }, 
+            handleSortButtonPressed: function () {
+                this.getViewSettingsDialog("ap.salesorderapp.fragments.sortDialog")
+                    .then(function (oViewSettingsDialog) {
+                        oViewSettingsDialog.open();
+                    });
             },
             handleSortDialogConfirm: function (oEvent) {
                 var oTable = this.byId("salesorderTable"),
